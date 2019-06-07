9d01cc53772ed73add8b07361a14a863966b1a88
samuraisoldier <24753407+samuraisoldier@users.noreply.github.com>
2019-06-07 18:23:53 +0100

    getting res

--- finallvnet.py
+++ finallvnet.py
@@ -29,7 +29,7 @@ res_feeder = pp.from_excel("final code/circuits/res_feeder.xlsx")
 #res_feeder
 #make some network plots
 #plot.simple_plot(res_feeder, show_plot=True, trafo_size = 1.5, plot_loads = True, plot_sgens = True)
-#simple_plotly(res_feeder)
+simple_plotly(res_feeder)
 
 
 ####COMMERCIAL FEEDER###########
@@ -66,10 +66,11 @@ mixed_feeder.measurement = pd.read_excel('final code/measurements/mixed_feeder_m
 ########################################################RUN ESTIMATORS########################################
 
 #####INDUSTRIAL ESTIMATOR#########
+ind_feeder.measurement = pd.read_excel('final code/measurements/ind_feeder_m.xlsx')
 successi = estimate(ind_feeder, init='flat')
 print(successi)
 #ind_feeder.measurement
-ind_feeder.res_line_est
+#ind_feeder.res_line_est
 ind_feeder.res_bus_est
 
 
@@ -77,10 +78,11 @@ ind_feeder.res_bus_est
 #####RESIDENTIAL ESTIMATOR########
 success = estimate(res_feeder, init='flat')
 print(success)
-res_feeder.res_line_est
-res_feeder.res_bus_est
+#res_feeder.res_line_est
+#res_feeder.res_bus_est
 
 #####COMMERCIAL ESTIMATOR#########
+#comm_feeder.measurement = pd.read_excel('final code/measurements/comm_feeder_m.xlsx')
 successc = estimate(comm_feeder, init='flat')
 print(successc)
 #comm_feeder.res_line_est
@@ -89,60 +91,68 @@ print(successc)
 #####MIXED ESTIMATOR##############
 successrc = estimate(mixed_feeder, init='flat')
 print(successrc)
-#mixed_feeder.res_line_est
-#mixed_feeder.res_bus_est
+mixed_feeder.res_line_est
+mixed_feeder.res_bus_est
 
 #################################################SET UP MEASUREMENTS FOR OVERALL FEEDER########################
 
 ##############################BUS MEASUREMENTS#####################################
-
+net = pp.from_excel("final code/circuits/net3.xlsx")
 ##RESIDENTIAL
 i = 0
 while i < len(res_feeder.res_bus_est):
     pp.create_measurement(net, 'v', 'bus', res_feeder.res_bus_est.vm_pu[i] , 0.0004, element = i+3)
-    pp.create_measurement(net, 'p', 'bus', res_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+3)
+    pp.create_measurement(net, 'p', 'bus', -res_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+3)
+    #pp.create_measurement(net, 'q', 'bus', res_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+3)
     i = i+ 1
 
 ##INDUSTRIAL
 i = 0
 while i < len(ind_feeder.res_bus_est):
     pp.create_measurement(net, 'v', 'bus', ind_feeder.res_bus_est.vm_pu[i] , 0.0004, element = i+22)
-    pp.create_measurement(net, 'p', 'bus', ind_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+22)
+    pp.create_measurement(net, 'p', 'bus', -ind_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+22)
+    #pp.create_measurement(net, 'q', 'bus', ind_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+22)
     i = i+ 1
 
 ##COMMERCIAL
 i = 0
 while i < len(comm_feeder.res_bus_est):
     pp.create_measurement(net, 'v', 'bus', comm_feeder.res_bus_est.vm_pu[i] , 0.0004, element = i+26)
-    pp.create_measurement(net, 'p', 'bus', comm_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+26)
+    pp.create_measurement(net, 'p', 'bus', -comm_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+26)
+    #pp.create_measurement(net, 'q', 'bus', comm_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+26)
     i = i+ 1
 
 ##MIXED
 i = 0
 while i < len(mixed_feeder.res_bus_est):
     pp.create_measurement(net, 'v', 'bus', mixed_feeder.res_bus_est.vm_pu[i] , 0.0004, element = i+47)
-    pp.create_measurement(net, 'p', 'bus', mixed_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+47)
+    pp.create_measurement(net, 'p', 'bus', -mixed_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+47)
+    #pp.create_measurement(net, 'q', 'bus', mixed_feeder.res_bus_est.p_mw[i] , 0.0010, element = i+47)
     i = i+ 1
 
 
 pp.create_measurement(net, 'v', 'bus', 1 , 0.0004, element = 0)
 pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 1)
 pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 1)
-pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 3)
-pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 22)
-pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 26)
-pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 47)
+#pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 2)
+pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 21)
+#pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 25)
+#pp.create_measurement(net, 'v', 'bus', np.random.normal(1, 0.0004), .0004,  element = 46)
 
 
-pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 26)
-pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 47)
+#pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 26)
+#pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 47)
 pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 22)
-pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 3)
-pp.create_measurement(net, 'q', 'bus', -1, .001,  element = 21)
-pp.create_measurement(net, 'q', 'bus', 1, .001,  element = 23)
-
-
-#net.measurement
+pp.create_measurement(net, 'p', 'bus', -8, .001,  element = 23)
+#pp.create_measurement(net, 'q', 'bus', -1, .001,  element = 24)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 22)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 24)
+pp.create_measurement(net, 'q', 'bus', .52, .001,  element = 46)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 4)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 3)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 47)
+pp.create_measurement(net, 'q', 'bus', 0, .001,  element = 48)
+net.measurement[30:40]
 
 #net.measurement.to_excel('final code/measurements/allmeasev.xlsx')
 ########################################################OVERALL ESTIMATOR########################################
@@ -165,7 +175,7 @@ net.res_bus_est.to_excel('final code/result/bus_ev.xlsx')
 
 #CHOOSE AND ALLEVIATE
 #INDUSTRIAL
-pp.create_measurement(net, 'p', 'bus', 0, .001,  element = 24)
+pp.create_measurement(net, 'p', 'bus', 3, .001,  element = 24)
 
 pp.create_measurement(net, 'p', 'bus', 5, .001,  element = 23)
 #COMMERCIAL
